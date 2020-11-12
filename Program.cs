2d96a3c63d40b32450e65065afa810102ee3f14e
Guilherme <guilhermeandre2010@gmail.com>
2020-11-12 14:40:41 -0300

    C# soma

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace PROJETO1
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Digite seu nome:");
-           string a = Console.ReadLine();
-           Console.WriteLine("Bem Vindo " + a);
+          Console.WriteLine("Digite o primeiro numero:");
+          string num1_texto = Console.ReadLine();
+          int num1 = int.Parse(num1_texto);
+
+          Console.WriteLine("Digite o segundo numero");
+          int num2 = int.Parse(Console.ReadLine());
+
+          int soma = num1 + num2;
+
+          Console.Write("O resultado é: ");
+          Console.WriteLine(soma);
         }
     }
 }
