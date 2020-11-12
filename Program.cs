4f8471b47d83588efecb0b1e9fd8897bfb547251
Guilherme <guilhermeandre2010@gmail.com>
2020-11-12 14:24:55 -0300

    c# nome

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PROJETO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Digite seu nome:");
+           string a = Console.ReadLine();
+           Console.WriteLine("Bem Vindo " + a);
+        }
+    }
+}
